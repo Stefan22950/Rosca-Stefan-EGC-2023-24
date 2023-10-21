e53506710a37f35ef01d95733043b5927c2e0a42
Stefan22950 <127902822+Stefan22950@users.noreply.github.com>
2023-10-21 22:52:43 +0300

    Laboratorul 2
    Exercitiul 2 si 3

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace Rosca
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (Window3D example = new Window3D())
+            {
+                example.Run(30.0, 0.0);
+            }
+        }
+    }
+}
